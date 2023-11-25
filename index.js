836b3eda3beaa189e4b3f9765aebc8d5401c137a
Polrambora <sandbox@polrambora.com>
2023-11-25 17:29:50 +0000

    Clean code, add modules

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import request from 'request'
 import Discord from 'discord.js' // Using discord.js v13.8
+import openaiAutomod from './src/automod';
 
 const client = new Discord.Client({
     intents: [
@@ -14,68 +14,30 @@ const client = new Discord.Client({
         Discord.Intents.FLAGS.DIRECT_MESSAGE_TYPING,
     ],
     partials: ["CHANNEL"]
-}); // You can change if you want to.
+});
 
-const OpenAI_KEY = process.env["API_KEY"]; // Use your own api key (MAKE SURE IT'S WORKING!)
-
+const OpenAI_KEY = process.env["API_KEY"];
+const automod = new openaiAutomod(OpenAI_KEY);
 
 client.on("ready", () => {
     console.log('I am ready!')
 
 })
 
-client.on("messageCreate", async message => {
-    if (message.channel.type === "DM") return; // So it won't error if automod has been triggered on Direct Messages.
+cclient.on("messageCreate", async (message) => {
+    if (message.channel.type === "DM") return;
 
-    // Self explanatory
-    const friendlytags = {
-        'sexual': "Sexual Content",
-        'hate': 'Hate Speech',
-        'violence': 'Violence',
-        'self-harm': "Self-Harm",
-        'sexual/minors': 'Child Endangerment',
-        'hate/threatening': 'Threatening',
-        'violence/graphic': 'Graphic Violence'
+    try {
+        const results = await automod.runAutomod(message.content);
+
+        if (results.isFlagged) {
+            console.log("Flagged Message");
+        } else {
+            console.log("Message is clear");
+        }
+    } catch (error) {
+        console.error("Automod error:", error);
     }
-
-    request.post({
-        url: `https://api.openai.com/v1/moderations`,
-        
-        headers: {
-            'Authorization': `Bearer ${apiKey}`,
-            'content-type': 'application/json'
-        },
-
-        body: JSON.stringify({
-            input: message.content
-        })
-
-    }, (err, response, body) => {
-        if (err) {
-            console.log("Error: ", err);
-        };
-
-        const ParsedResponseBody = JSON.parse(body)
-
-        if (ParsedResponseBody.results[0].flagged === true) {
-
-            let sortedEntries = Object.entries(ParsedResponseBody.results[0].categories).sort((a, b) => b[0] - a[0]);
-            console.log(sortedEntries)
-
-            for (let i = 0; i < sortedEntries.length; i++) {
-                if (sortedEntries[i][1] == true) {
-                    var value = sortedEntries[i][0];
-                };
-            };
-
-            message.member.timeout(3600000, friendlytags[value]).then(() => {
-                // Put your code here or use this:
-                message.delete().catch(console.error)
-                message.author.send(`Woah there! It seems you sent a message that contains ${friendlytags[value]}\nYou have been timed out for 1 hour.`)
-
-            });
-        };
-    });
-})
+});
 
 client.login(process.env["TOKEN"])
